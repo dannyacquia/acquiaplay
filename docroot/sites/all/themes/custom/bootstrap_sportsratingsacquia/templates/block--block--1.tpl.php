b48d6267515e5022e67011d50f4340bb7458b40d
Nick DelRossi <nickwtf@gmail.com>
2015-06-19 08:25:49 -0400

    Start theming the welcome block

---
+++ docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/templates/block--block--1.tpl.php
@@ -0,0 +1,37 @@
+<?php
+// @TODO Need to move data into variables
+?>
+
+<div class="container welcome">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="box">
+                <div class="info">
+                    <h4 class="text-center">Sign In</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                    <a href="" class="btn">Link</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="box">
+                <div class="info">
+                    <h4 class="text-center">Play</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                    <a href="" class="btn">Link</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <div class="box">
+                <div class="info">
+                    <h4 class="text-center">Post</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                    <a href="" class="btn">Link</a>
+                </div>
+            </div>
+        </div>
+  </div>
+</div>
