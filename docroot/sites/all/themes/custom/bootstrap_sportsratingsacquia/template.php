4f50b099594db15b8e9e04f46e8d162f5588bc30
Nick DelRossi <nickwtf@gmail.com>
2015-06-20 12:04:41 -0400

    Update user profile page. Alter page title. right align tds. add some comments

--- docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/template.php
+++ docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/template.php
@@ -5,25 +5,39 @@
  * template.php
  */
 
+/*
+ * Implements template_preprocess_page
+ */
 function bootstrap_sportsratingsacquia_preprocess_page(&$variables, $hook) {
 
-  // Remove front page welcome message
-  if (drupal_is_front_page()) { $variables['title']=""; }
+    // Remove front page welcome message
+    if (drupal_is_front_page()) {
+        drupal_set_title("");
+    }
 
+    // Removes '.' from title and capitalizes
+    // Only for user page
+    $path = drupal_get_path_alias();
+    if ((strpos($path, 'user') !== FALSE)) {
+        $new_title = ucwords(str_replace('.', ' ', drupal_get_title()));
+        drupal_set_title($new_title);
+    }
 }
 
+/*
+ * Implements template_preprocess_views_view_table
+ *
+ * Changes table to use different css classes
+ */
 function bootstrap_sportsratingsacquia_preprocess_views_view_table(&$vars)
 {
     $vars['classes_array'][] = 'table table-striped';
 }
 
 /**
- * Process variables for user-profile.tpl.php.
+ * Implements template_preprocess_views_view_table
  *
- * The $variables array contains the following arguments:
- * - $account
- *
- * @see user-profile.tpl.php
+ * Adds variables for use in user template page
  */
 function bootstrap_sportsratingsacquia_preprocess_user_profile(&$variables) {
     $account = $variables['elements']['#account'];
@@ -31,8 +45,10 @@ function bootstrap_sportsratingsacquia_preprocess_user_profile(&$variables) {
     foreach (element_children($variables['elements']) as $key) {
         $variables['user_profile'][$key] = $variables['elements'][$key];
     }
-    //Add mail to $user_profile variable
+    //Add mail and name to $user_profile variable
     $variables['user_profile']['mail'] = $account->mail;
+    $variables['user_profile']['name'] = $account->name;
     // Preprocess fields.
     field_attach_preprocess('user', $account, $variables['elements'], $variables);
 }
+
