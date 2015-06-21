d49441a5a59b1a74b4fa8a5030e017aa6a1a8737
Nick DelRossi <nickwtf@gmail.com>
2015-06-21 09:52:11 -0400

    Update welcome node on front page. Reduce cover image height.

--- docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/templates/block--block--1.tpl.php
+++ docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/templates/block--block--1.tpl.php
@@ -2,35 +2,29 @@
 // @TODO Need to move data into variables
 ?>
 
-<h2 class="text-center welcome-message">Welcome to Acquia Play</h2>
+<div class="welcome">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="well text-center">
+        <strong><?php print variable_get('site_slogan', 'Welcome to Acquia Play - Leaderboards for ping pong and foosball'); ?></strong>
+      </div>
+    </div>
+  </div>
 
-<div class="container welcome">
-    <div class="row col-lg-10 col-lg-offset-1">
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="box">
-                <div class="info">
-                    <h4 class="text-center">Sign In</h4>
-                    <p>Sign in with your Acquia Google account. You will need an acquia.com email address to use this site.</p>
-                </div>
-            </div>
-        </div>
+  <div class="row col-md-10 col-md-offset-1">
+    <div class="col-md-4">
+      <h2>Sign In</h2>
+      <p>Sign in with your Acquia Google account. You will need an acquia.com email address to use this site.</p>
+    </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="box">
-                <div class="info">
-                    <h4 class="text-center">Play</h4>
-                    <p>Find someone on the leaderboards and challenge them to a ping pong or foosball game.</p>
-                </div>
-            </div>
-        </div>
+    <div class="col-md-4">
+      <h2>Play</h2>
+      <p>Find someone on the ping pong or foosball leaderboards and challenge them to a game.</p>
+    </div>
 
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="box">
-                <div class="info">
-                    <h4 class="text-center">Post</h4>
-                    <p>When your game is complete the winner should click the "Post match results" button on the leaderboard pages.</p>
-                </div>
-            </div>
-        </div>
+    <div class="col-md-4">
+      <h2>Post</h2>
+      <p>When your game is complete the winner should click the "Post match results" button on the leaderboard pages.</p>
+    </div>
   </div>
 </div>
