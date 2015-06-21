aa908a172a47a0a2dbd32a50506609779a6a16d4
Nick DelRossi <nickwtf@gmail.com>
2015-06-21 15:40:40 -0400

    Center headers on welcome block

--- docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/templates/block--block--1.tpl.php
+++ docroot/sites/all/themes/custom/bootstrap_sportsratingsacquia/templates/block--block--1.tpl.php
@@ -13,17 +13,17 @@
 
   <div class="row col-md-10 col-md-offset-1">
     <div class="col-md-4">
-      <h2>Sign In</h2>
+      <h2 class="text-center">Sign In</h2>
       <p>Sign in with your Acquia Google account. You will need an acquia.com email address to use this site.</p>
     </div>
 
     <div class="col-md-4">
-      <h2>Play</h2>
+      <h2 class="text-center">Play</h2>
       <p>Find someone on the ping pong or foosball leaderboards and challenge them to a game.</p>
     </div>
 
     <div class="col-md-4">
-      <h2>Post</h2>
+      <h2 class="text-center">Post</h2>
       <p>When your game is complete the winner should click the "Post match results" button on the leaderboard pages.</p>
     </div>
   </div>
